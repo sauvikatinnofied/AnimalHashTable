cf54e8bb80d0044c8f314a6b79fb014d9e4d7005
Sandip Saha <innofied@Sandips-Mac-mini.local>
2013-07-16 17:12:06 +0530

    Initial Commit

---
+++ AnimalHashTable/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AnimalHashTable
+//
+//  Created by Sandip Saha on 16/07/13.
+//  Copyright (c) 2013 Sandip Saha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
